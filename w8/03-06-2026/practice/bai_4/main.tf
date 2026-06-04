81332a44a1ca34123cbc9fc122142b54d5a688ee
donan2k5 <tuphucnguyen20051@gmail.com>
2026-06-04 16:49:25 +0700

    feat: add 03-06 k8s lab test

---
+++ w8/03-06-2026/practice/bai_4/main.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "My VPC"
+  }
+}
+
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "My Subnet"
+  }
+}
+
